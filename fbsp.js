d2aa2e28d0423aa63a956b34dcc495635449ac31
Eric Schrijver <eric@ericschrijver.nl>
2012-12-10 01:04:26 +0100

    The humble beginnings of the FBSP2013 page
    Took your html my ♥ and tried to turn it into
    a Meteor app

---
+++ fbsp.js
@@ -0,0 +1,33 @@
+Comments = new Meteor.Collection("comments");
+
+if (Meteor.isClient) {
+
+  Template.comments.comments = function () {
+    return Comments.find({});
+  };
+
+  Template.form.events({
+    'click input#submit' : function () {
+      if (typeof console !== 'undefined')
+        Comments.insert({   status: "student",
+                            details: $('#details').val(),
+                            public: true,
+                            email: $('#email').val()
+        });
+    return false;
+    }
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Comments.find().count() === 0) {
+        Comments.insert({   status: "student",
+                            details: "There are no comments yet. this is a default.",
+                            public: true,
+                            email: "eric@example.com"
+        });
+    }
+  });
+}
