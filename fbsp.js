bcebd26f5189d3e8e9fd57c128ec7464306763cb
Furter <loraine.furter@gmail.com>
2013-02-25 11:00:40 +0100

    Links in new tab + clean links urls etc
    Next episode: the great platform!!!

--- fbsp.js
+++ fbsp.js
@@ -55,6 +55,34 @@ Template.collective_editing.comments = function () {
     return Comments.find();
   };
   
+  
+/* to open links in new tabs, but not the internal links
+     */
+var isExternal = function(href) {
+    /*
+    "-1" = rien trouvé
+    "127.0.0.1" = localhost
+    "===" = est
+    "!==" = n'est pas (donc "!== -1" = n'a pas rien trouvé ;)
+    dans la console: document.location.host [enter] -> trouve quel url c'est
+    dans indexOf(document.location.host), document.location.host c'est le host/url du site (ou localhost)
+     * isExternal("http://autre-site.com/")
+     * true
+     * isExternal("/path/")
+     * false
+     * isExternal("http://spion.me/publications/")
+     * false
+     * isExternal("http://localhost:8000/publications/")
+     * false
+     * isExternal("http://127.0.0.1:8000/publications/")
+     * false
+     */
+    if (href.indexOf("http") === -1 || href.indexOf(document.location.host) !== -1 || href.indexOf("localhost") !== -1 || href.indexOf("127.0.0.1") !== -1 ) {
+        return false;
+    }
+    return true;
+};
+ 
 
 Meteor.startup(function () {
     $('a').smoothScroll();
@@ -68,7 +96,18 @@ $('#news').click(function(event){
 });
 
 
+// une fois que tout la structure de la page est là: action
 $(document).ready(function() {
+    
+    // action: si c'est externe, on attribue à "ça" une target pour le lien qui est un nouvel onglet 
+    $("a[href]").each(
+    function() { 
+        if (isExternal($(this).attr('href')) ) { 
+            $(this).attr('target', '_blank')
+            }
+        }
+    );
+    
     // add a new comment
     $("#collective-editing").click(function(e) {
         e.preventDefault();
